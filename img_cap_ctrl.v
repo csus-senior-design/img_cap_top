1b92aa18f4b5fa8f29b818ce7f99dcbad3821fed
Greg M. Crist, Jr <gmcrist@gmail.com>
2015-08-21 09:58:53 -0700

    Integrating top-level module with submodules; Added init module and base for top-level control

--- img_cap_ctrl.v
+++ img_cap_ctrl.v
@@ -1,11 +1,81 @@
-module ctrl #(
-	parameter	DEF_VAL = 1'b0
-)(
-	input		clk,
-				rst,
-				start,
-				full,
-	output	reg	wr_en,
-				reg	rd_en
-);
+/*
+----------------------------------------
+Stereoscopic Vision System
+Senior Design Project - Team 11
+California State University, Sacramento
+Spring 2015 / Fall 2015
+----------------------------------------
 
+Image Capture Controller
+
+Authors:  Greg M. Crist, Jr. (gmcrist@gmail.com)
+
+Description:
+    Top-Level Control State Machine
+*/
+module img_cap_ctrl #(
+        parameter PLACEHOLDER = 0
+    )(
+        input  clk,
+        input  reset,
+        input  init_done,
+        output reg   init_start
+    );
+
+    localparam s_idle       = 0,
+               s_reset      = 1,
+               s_init       = 2,
+               s_init_wait  = 3,
+               s_init_done  = 4,
+               s_fb_prefill = 5,
+               s_fb_stream  = 6;
+               
+    (* syn_encoding = "safe" *)
+    reg [3:0] state;
+    
+    always @ (posedge clk) begin
+        if (~reset) begin
+            state <= s_reset;
+            init_start <= 1'b0;
+        end
+        else begin
+            case (state)
+                // Placeholder for just chilling out
+                s_idle: begin
+                end
+
+                // When put in a reset state, just jump straight to init
+                s_reset: begin
+                    state <= s_init;
+                end
+                
+                // Kickoff initialization of system components
+                s_init: begin
+                    state <= s_init_wait;
+                    init_start <= 1'b1;
+                end
+                
+                // Wait for system components to finish initializing
+                s_init_wait: begin
+                    state <= init_done ? s_init_done : s_init_wait;
+                    init_start <= 1'b0;
+                end
+                
+                // Transition from initialization to filling the framebuffers
+                s_init_done: begin
+                    state <= s_fb_prefill;
+                end
+                
+                // Pre-fill the camera input framebuffers before pushing forward
+                s_fb_prefill: begin
+                    state <= s_fb_stream;
+                end
+               
+                // Stream from the first framebuffer stage to the next and enable outputs
+                s_fb_stream: begin
+                    state <= s_idle;
+                end
+            endcase
+        end
+    end
+endmodule
--- img_cap_top.v
+++ img_cap_top.v
@@ -17,199 +17,204 @@ Description:
 
 `timescale 1 ns / 1 ns
 
-module img_cap_top (
-	// Clocks (obviously)
-	(*
-		chip_pin = "R20",
-		altera_attribute = "-name IO_STANDARD \"3.3-V LVTTL\""
-	*)
-	input           CLOCK_50_B5B,
-	(*
-		chip_pin = "N20",
-		altera_attribute = "-name IO_STANDARD \"3.3-V LVTTL\""
-	*)
-	input           CLOCK_50_B6A,
+module img_cap_top #(
+        parameter  CAM1_CHIP_ADDR = 8'hCD,
+                   CAM2_CHIP_ADDR = 8'hCD,
+                   ADV7513_CHIP_ADDR = 7'h39,
+                   ADV7513_I2C_CLKDIV = 12'd125
+    )(
+        // Clocks (obviously)
+        (*
+            chip_pin = "R20",
+            altera_attribute = "-name IO_STANDARD \"3.3-V LVTTL\""
+        *)
+        input           CLOCK_50_B5B,
+        (*
+            chip_pin = "N20",
+            altera_attribute = "-name IO_STANDARD \"3.3-V LVTTL\""
+        *)
+        input           CLOCK_50_B6A,
 
-	// Reset (super obvious)
-	(*
-		chip_pin = "AB24",
-		altera_attribute = "-name IO_STANDARD \"3.3-V LVTTL\""
-	*)
-	input           CPU_RESET_n,
+        // Reset (super obvious)
+        (*
+            chip_pin = "AB24",
+            altera_attribute = "-name IO_STANDARD \"3.3-V LVTTL\""
+        *)
+        input           CPU_RESET_n,
 
-	// HDMI-TX via ADV7513
-	(*
-		chip_pin = "Y25",
-		altera_attribute = "-name IO_STANDARD \"3.3-V LVTTL\""
-	*)
-	output          HDMI_TX_CLK,
-	(*
-		chip_pin = "Y26",
-		altera_attribute = "-name IO_STANDARD \"3.3-V LVTTL\""
-	*)
-	output          HDMI_TX_DE,
-	(*
-		chip_pin = "U26",
-		altera_attribute = "-name IO_STANDARD \"3.3-V LVTTL\""
-	*)
-	output          HDMI_TX_HS,
-	(*
-		chip_pin = "U25",
-		altera_attribute = "-name IO_STANDARD \"3.3-V LVTTL\""
-	*)
-	output          HDMI_TX_VS,
-	(*
-		chip_pin = "AD25, AC25, AB25, AA24, AB26, R26, R24, P21, P26, N25, P23, P22, R25, R23, T26, T24, T23, U24, V25, V24, W26, W25, AA26, V23",
-		altera_attribute = "-name IO_STANDARD \"3.3-V LVTTL\""
-	*)
-	output  [23:0]  HDMI_TX_D,
-	(*
-		chip_pin = "T12",
-		altera_attribute = "-name IO_STANDARD \"1.2 V\""
-	*)
-	input           HDMI_TX_INT,
+        // HDMI-TX via ADV7513
+        (*
+            chip_pin = "Y25",
+            altera_attribute = "-name IO_STANDARD \"3.3-V LVTTL\""
+        *)
+        output          HDMI_TX_CLK,
+        (*
+            chip_pin = "Y26",
+            altera_attribute = "-name IO_STANDARD \"3.3-V LVTTL\""
+        *)
+        output          HDMI_TX_DE,
+        (*
+            chip_pin = "U26",
+            altera_attribute = "-name IO_STANDARD \"3.3-V LVTTL\""
+        *)
+        output          HDMI_TX_HS,
+        (*
+            chip_pin = "U25",
+            altera_attribute = "-name IO_STANDARD \"3.3-V LVTTL\""
+        *)
+        output          HDMI_TX_VS,
+        (*
+            chip_pin = "AD25, AC25, AB25, AA24, AB26, R26, R24, P21, P26, N25, P23, P22, R25, R23, T26, T24, T23, U24, V25, V24, W26, W25, AA26, V23",
+            altera_attribute = "-name IO_STANDARD \"3.3-V LVTTL\""
+        *)
+        output  [23:0]  HDMI_TX_D,
+        (*
+            chip_pin = "T12",
+            altera_attribute = "-name IO_STANDARD \"1.2 V\""
+        *)
+        input           HDMI_TX_INT,
 
-	// External I2C bus for HDMI-TX
-	(*
-		chip_pin = "B7",
-		altera_attribute = "-name IO_STANDARD \"2.5 V\""
-	*)
-	inout           I2C_SCL,
-	(*
-		chip_pin = "G11",
-		altera_attribute = "-name IO_STANDARD \"2.5 V\""
-	*)
-	inout           I2C_SDA,
+        // External I2C bus for HDMI-TX
+        (*
+            chip_pin = "B7",
+            altera_attribute = "-name IO_STANDARD \"2.5 V\""
+        *)
+        inout           I2C_SCL,
+        (*
+            chip_pin = "G11",
+            altera_attribute = "-name IO_STANDARD \"2.5 V\""
+        *)
+        inout           I2C_SDA,
 
-	// Status LEDs
-	(*
-		chip_pin = "J10, H7, K8, K10, J7, J8, G7, G6, F6, F7",
-		altera_attribute = "-name IO_STANDARD \"2.5 V\""
-	*)
-	output  [9:0]   LEDR,
-	(*
-		chip_pin = "H9, H8, B6, A5, E9, D8, K6, L7",
-		altera_attribute = "-name IO_STANDARD \"2.5 V\""
-	*)
-	output  [7:0]   LEDG,
+        // Status LEDs
+        (*
+            chip_pin = "J10, H7, K8, K10, J7, J8, G7, G6, F6, F7",
+            altera_attribute = "-name IO_STANDARD \"2.5 V\""
+        *)
+        output  [9:0]   LEDR,
+        (*
+            chip_pin = "H9, H8, B6, A5, E9, D8, K6, L7",
+            altera_attribute = "-name IO_STANDARD \"2.5 V\""
+        *)
+        output  [7:0]   LEDG,
 
-	// Debounced push buttons
-	(*
-		chip_pin = "Y16, Y15, P12, P11",
-		altera_attribute = "-name IO_STANDARD \"1.2 V\""
-	*)
-	input   [3:0]   KEY,
-
-
-	// Camera Interfaces
-
-	// Shared Pins
-	(*
-		chip_pin = "F26",
-		altera_attribute = "-name IO_STANDARD \"3.3-V LVTTL\""
-	*)
-	output          CAM_XCLK,   // Cam 1 & 2 XCLK (GPIO 16)
-
-	(*
-		chip_pin = "W20",
-		altera_attribute = "-name IO_STANDARD \"3.3-V LVTTL\""
-	*)
-	output          CAM_RESET,  // Cam 1 & 2 RESET (GPIO 28)
-
-	(*
-		chip_pin = "G26",
-		altera_attribute = "-name IO_STANDARD \"3.3-V LVTTL\""
-	*)
-	output          CAM_PWDN,   // Cam 1 & 2 PWDN (GPIO 18)
-
-	// CAM 1 Pins
-	(*
-		chip_pin = "T21",
-		altera_attribute = "-name IO_STANDARD \"3.3-V LVTTL\""
-	*)
-	input           CAM1_PCLK,  // Cam 1 PCLK (GPIO 0)
-
-	(*
-		chip_pin = "U19",
-		altera_attribute = "-name IO_STANDARD \"3.3-V LVTTL\""
-	*)
-	input           CAM1_HREF,  // Cam 1 HREF (GPIO 10)
-
-	(*
-		chip_pin = "U22",
-		altera_attribute = "-name IO_STANDARD \"3.3-V LVTTL\""
-	*)
-	input           CAM1_VSYNC, // Cam 1 VSYNC (GPIO 11)
-
-	(*
-		chip_pin = "T19",
-		altera_attribute = "-name IO_STANDARD \"3.3-V LVTTL\""
-	*)
-	output          CAM1_SDIOC, // Cam 1 SDIOC (GPIO 9)
-
-	(*
-		chip_pin = "P8",
-		altera_attribute = "-name IO_STANDARD \"3.3-V LVTTL\""
-	*)
-	inout           CAM1_SDIOD, // Cam 1 SDIOD (GPIO 12)
-
-	(*
-		chip_pin = "P20, R9, M26, T22, E26, M21, D26, K26",
-		altera_attribute = "-name IO_STANDARD \"3.3-V LVTTL\""
-	*)
-	input   [7:0]   CAM1_DATA,      // Cam 1 Data (GPIO 7, 14, 5, 8, 3, 6, 1, 4)
+        // Debounced push buttons
+        (*
+            chip_pin = "Y16, Y15, P12, P11",
+            altera_attribute = "-name IO_STANDARD \"1.2 V\""
+        *)
+        input   [3:0]   KEY,
 
 
-	// CAM 2 Pins
-	(*
-		chip_pin = "K25",
-		altera_attribute = "-name IO_STANDARD \"3.3-V LVTTL\""
-	*)
-	input           CAM2_PCLK,  // Cam 2 PCLK (GPIO 2)
+        // Camera Interfaces
 
-	(*
-		chip_pin = "V20",
-		altera_attribute = "-name IO_STANDARD \"3.3-V LVTTL\""
-	*)
-	input           CAM2_HREF,  // Cam 2 HREF (GPIO 26)
+        // Shared Pins
+        (*
+            chip_pin = "F26",
+            altera_attribute = "-name IO_STANDARD \"3.3-V LVTTL\""
+        *)
+        output          CAM_XCLK,   // Cam 1 & 2 XCLK (GPIO 16)
 
-	(*
-		chip_pin = "R8",
-		altera_attribute = "-name IO_STANDARD \"3.3-V LVTTL\""
-	*)
-	input           CAM2_VSYNC, // Cam 2 VSYNC (GPIO 13)
+        (*
+            chip_pin = "W20",
+            altera_attribute = "-name IO_STANDARD \"3.3-V LVTTL\""
+        *)
+        output          CAM_RESET,  // Cam 1 & 2 RESET (GPIO 28)
 
-	(*
-		chip_pin = "R10",
-		altera_attribute = "-name IO_STANDARD \"3.3-V LVTTL\""
-	*)
-	output          CAM2_SDIOC, // Cam 2 SDIOC (GPIO 15)
+        (*
+            chip_pin = "G26",
+            altera_attribute = "-name IO_STANDARD \"3.3-V LVTTL\""
+        *)
+        output          CAM_PWDN,   // Cam 1 & 2 PWDN (GPIO 18)
 
-	(*
-		chip_pin = "W21",
-		altera_attribute = "-name IO_STANDARD \"3.3-V LVTTL\""
-	*)
-	inout           CAM2_SDIOD, // Cam 2 SDIOD (GPIO 27)
+        // CAM 1 Pins
+        (*
+            chip_pin = "T21",
+            altera_attribute = "-name IO_STANDARD \"3.3-V LVTTL\""
+        *)
+        input           CAM1_PCLK,  // Cam 1 PCLK (GPIO 0)
 
-	(*
-		chip_pin = "V22, AA7, AD6, AD7, AA6, U20, Y8, Y9",
-		altera_attribute = "-name IO_STANDARD \"3.3-V LVTTL\""
-	*)
-	input   [7:0]   CAM2_DATA,  // Cam 2 Data (GPIO 25, 20, 23, 22, 21, 24, 19, 17)
+        (*
+            chip_pin = "U19",
+            altera_attribute = "-name IO_STANDARD \"3.3-V LVTTL\""
+        *)
+        input           CAM1_HREF,  // Cam 1 HREF (GPIO 10)
+
+        (*
+            chip_pin = "U22",
+            altera_attribute = "-name IO_STANDARD \"3.3-V LVTTL\""
+        *)
+        input           CAM1_VSYNC, // Cam 1 VSYNC (GPIO 11)
+
+        (*
+            chip_pin = "T19",
+            altera_attribute = "-name IO_STANDARD \"3.3-V LVTTL\""
+        *)
+        output          CAM1_SDIOC, // Cam 1 SDIOC (GPIO 9)
+
+        (*
+            chip_pin = "P8",
+            altera_attribute = "-name IO_STANDARD \"3.3-V LVTTL\""
+        *)
+        inout           CAM1_SDIOD, // Cam 1 SDIOD (GPIO 12)
+
+        (*
+            chip_pin = "P20, R9, M26, T22, E26, M21, D26, K26",
+            altera_attribute = "-name IO_STANDARD \"3.3-V LVTTL\""
+        *)
+        input   [7:0]   CAM1_DATA,      // Cam 1 Data (GPIO 7, 14, 5, 8, 3, 6, 1, 4)
 
 
-	// Memory ports
-	output  [9:0]   mem_ca,
-	output  [0:0]   mem_ck,
-	output  [0:0]   mem_ck_n,
-	output  [0:0]   mem_cke,
-	output  [0:0]   mem_cs_n,
-	output  [3:0]   mem_dm,
-	inout   [31:0]  mem_dq,
-	inout   [3:0]   mem_dqs,
-	inout   [3:0]   mem_dqs_n,
-	input           oct_rzqin
-);
+        // CAM 2 Pins
+        (*
+            chip_pin = "K25",
+            altera_attribute = "-name IO_STANDARD \"3.3-V LVTTL\""
+        *)
+        input           CAM2_PCLK,  // Cam 2 PCLK (GPIO 2)
+
+        (*
+            chip_pin = "V20",
+            altera_attribute = "-name IO_STANDARD \"3.3-V LVTTL\""
+        *)
+        input           CAM2_HREF,  // Cam 2 HREF (GPIO 26)
+
+        (*
+            chip_pin = "R8",
+            altera_attribute = "-name IO_STANDARD \"3.3-V LVTTL\""
+        *)
+        input           CAM2_VSYNC, // Cam 2 VSYNC (GPIO 13)
+
+        (*
+            chip_pin = "R10",
+            altera_attribute = "-name IO_STANDARD \"3.3-V LVTTL\""
+        *)
+        output          CAM2_SDIOC, // Cam 2 SDIOC (GPIO 15)
+
+        (*
+            chip_pin = "W21",
+            altera_attribute = "-name IO_STANDARD \"3.3-V LVTTL\""
+        *)
+        inout           CAM2_SDIOD, // Cam 2 SDIOD (GPIO 27)
+
+        (*
+            chip_pin = "V22, AA7, AD6, AD7, AA6, U20, Y8, Y9",
+            altera_attribute = "-name IO_STANDARD \"3.3-V LVTTL\""
+        *)
+        input   [7:0]   CAM2_DATA,  // Cam 2 Data (GPIO 25, 20, 23, 22, 21, 24, 19, 17)
+
+
+        // Memory ports
+        output  [9:0]   mem_ca,
+        output  [0:0]   mem_ck,
+        output  [0:0]   mem_ck_n,
+        output  [0:0]   mem_cke,
+        output  [0:0]   mem_cs_n,
+        output  [3:0]   mem_dm,
+        inout   [31:0]  mem_dq,
+        inout   [3:0]   mem_dqs,
+        inout   [3:0]   mem_dqs_n,
+        input           oct_rzqin
+    );
 
 	/* Pull down the LEDs for now (will use in control later) */
 	assign LEDR[9:1] = 9'h0;
@@ -441,5 +446,56 @@ module img_cap_top (
         .addr(CAM2_CAP_ADDRESS),
         .data_out(CAM2_CAP_DATA),
         .write_en(CAM2_CAP_WRITE_EN)
+    );
+
+    
+    // Top-level Control Modules
+    wire cam1_pwdn;
+    wire cam2_pwdn;
+    
+    wire init_start;
+    wire init_done;
+    
+    // To do: generate a 100kHz clock signal for this
+    wire clk_sccb;
+    
+    // To do: should we change this to AND?
+    assign CAM_PWDN = cam1_pwdn || cam2_pwdn;
+    
+    sys_init #(
+        .CAM1_CHIP_ADDR(CAM1_CHIP_ADDR),
+        .CAM2_CHIP_ADDR(CAM2_CHIP_ADDR),
+        .ADV7513_CHIP_ADDR(ADV7513_CHIP_ADDR),
+        .ADV7513_I2C_CLKDIV(ADV7513_I2C_CLKDIV)
+    ) sys_init (
+        .clk(clk_25_2m),
+        .clk_sccb(clk_sccb),
+        .reset(reset),
+        .start(init_start),
+        .done(init_done),
+        
+        // SCCB for Camera 1 Initialization 
+        .cam1_pwdn(cam1_pwdn),
+        .cam1_sio_d(CAM1_SDIOD),
+        .cam1_sio_c(CAM1_SDIOC),
+        
+        // SCCB for Camera 2 Initialization 
+        .cam2_pwdn(cam2_pwdn),
+        .cam2_sio_c(CAM2_SDIOD),
+        .cam2_sio_d(CAM2_SDIOC),
+
+        // I2C for ADV7513 Initialization
+        .i2c_scl(I2C_SCL),
+        .i2c_sda(I2C_SDA),
+
+        // Memory init done input
+        .mem_init_done(ram_rdy)
+    );
+    
+    img_cap_ctrl img_cap_ctrl (
+        .clk(clk_25_2m),
+        .reset(reset),
+        .init_start(init_start),
+        .init_done(init_done)
     );
 endmodule
